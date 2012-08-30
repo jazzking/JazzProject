40e913d1a3ff0effe17a7d31626c9c7fde33bb9b
jazzking <kitaek.kim@gmail.com>
2012-08-30 17:37:49 +0900

    Commit -17:37

--- first/GitTest_to_GitHub.java
+++ first/GitTest_to_GitHub.java
@@ -3,5 +3,6 @@ package de.vogella.git.first;
 public class GitTest {
 	public static void main(String[] args) {
 	    System.out.println("Git is Awesome!!!! to GitHub");
+	    System.out.println("김기택이가 Local에서 GitTest_to_GitHub.java를 수정함");
 	  }
 }

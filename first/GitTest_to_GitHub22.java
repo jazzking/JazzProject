fb269a1131a2b19fd96c825045ec10624c0ee119
jazzking <kitaek.kim@gmail.com>
2012-08-30 13:59:03 +0900

    for 2nd. Upload Test,

---
+++ first/GitTest_to_GitHub22.java
@@ -0,0 +1,14 @@
+package de.vogella.git.first;
+
+public class GitTest {
+	public static void main(String[] args) {
+	    System.out.println("Git is Awesome!!!! to GitHub");
+	    System.out.println("Git is Awesome2!!!! to GitHub");
+	    System.out.println("Git is Awesome3!!! to GitHub");
+	    System.out.println("Git is Awesome4!!! to GitHub");
+	    System.out.println("Git is Awesome5!!! to GitHub");
+	    System.out.println("Git is Awesome6!!! to GitHub");
+	    System.out.println("Git is Awesome7!!! to GitHub");
+	    System.out.println("Git is Awesome8!!! to GitHub");
+	  }
+}

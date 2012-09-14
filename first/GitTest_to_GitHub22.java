74d58d6a5fbeca790b1e35432858211d29dae1aa
jazzking <kitaek.kim@gmail.com>
2012-09-14 16:33:30 +0900

    Git Push Test from local to GitHub

--- first/GitTest_to_GitHub22.java
+++ first/GitTest_to_GitHub22.java
@@ -6,9 +6,6 @@ public class GitTest {
 	    System.out.println("Git is Awesome2!!!! to GitHub");
 	    System.out.println("Git is Awesome3!!! to GitHub");
 	    System.out.println("Git is Awesome4!!! to GitHub");
-	    System.out.println("Git is Awesome5!!! to GitHub");
-	    System.out.println("Git is Awesome6!!! to GitHub");
-	    System.out.println("Git is Awesome7!!! to GitHub");
-	    System.out.println("Git is Awesome8!!! to GitHub");
+	    System.out.println("Git Push Test 120914");
 	  }
 }

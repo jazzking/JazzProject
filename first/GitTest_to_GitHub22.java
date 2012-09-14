f811c3ec86a59f442277fc4a91ea050c122160ac
Kim, Ki Taek <kitaek.kim@gmail.com>
2012-09-14 16:52:17 +0900

    Update first/GitTest_to_GitHub22.java

--- first/GitTest_to_GitHub22.java
+++ first/GitTest_to_GitHub22.java
@@ -7,5 +7,7 @@ public class GitTest {
 	    System.out.println("Git is Awesome3!!! to GitHub");
 	    System.out.println("Git is Awesome4!!! to GitHub");
 	    System.out.println("Git Push Test 120914");
+            System.out.println("add comments in GitHub");
+
 	  }
 }

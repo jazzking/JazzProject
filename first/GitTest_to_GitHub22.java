32e4c52108644c752985e18db3a123f9abf1c685
jazzking <kitaek.kim@gmail.com>
2012-09-17 11:37:39 +0900

    Add line Git Push Test 120917 commit

--- first/GitTest_to_GitHub22.java
+++ first/GitTest_to_GitHub22.java
@@ -3,11 +3,10 @@ package de.vogella.git.first;
 public class GitTest {
 	public static void main(String[] args) {
 	    System.out.println("Git is Awesome!!!! to GitHub");
-	    System.out.println("Git is Awesome2!!!! to GitHub");
 	    System.out.println("Git is Awesome3!!! to GitHub");
-	    System.out.println("Git is Awesome4!!! to GitHub");
 	    System.out.println("Git Push Test 120914");
-            System.out.println("add comments in GitHub");
+	    System.out.println("Add line Git Push Test 120917");
+        System.out.println("add comments in GitHub");
 
 	  }
 }
